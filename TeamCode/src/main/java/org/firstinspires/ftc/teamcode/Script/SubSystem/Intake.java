af6cb003867517b3582f11a1a499a1b5ef17f216
olusen <olusen514@gmail.com>
2026-09-26 05:53:19 -0400

    Finished Intake.java with callable public classes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Script/SubSystem/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Script/SubSystem/Intake.java
@@ -1,4 +1,22 @@
 package org.firstinspires.ftc.teamcode.Script.SubSystem;
 
 public class Intake {
+
+    private final ROBOT robot;
+    private final double INTAKE_SPEED = 1.0;
+
+    // Constructor: Accepts your ROBOT instance so it can access robot.intake
+    public Intake(ROBOT robot) {
+        this.robot = robot;
+    }
+
+    // Turns Intake On (INTAKE_SPEED)
+    public void intake_On() {
+        robot.intake.setPower(INTAKE_SPEED);
+    }
+
+    // Turns Intake Off (0)
+    public void intake_Off() {
+        robot.intake.setPower(0);
+    }
 }
