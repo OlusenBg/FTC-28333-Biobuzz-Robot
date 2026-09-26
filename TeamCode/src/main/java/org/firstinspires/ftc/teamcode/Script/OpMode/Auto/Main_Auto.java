9cb2945a220718cd78bebb7c715995fa47aff925
olusen <olusen514@gmail.com>
2026-09-26 06:09:18 -0400

    Added OpModes Java Classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Script/OpMode/Auto/Main_Auto.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.Script.OpMode.Auto;
+
+public class Main_Auto {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Script/OpMode/Teleop/Main_Teleop.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.Script.OpMode.Teleop;
+
+public class Main_Teleop {
+}
